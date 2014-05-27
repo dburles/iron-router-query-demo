13544106915eff92979e82fb8fd6134d8233efc4
David Burles <dburles@gmail.com>
2014-05-27 19:50:58 +1000

    initial

---
+++ client/application.js
@@ -0,0 +1,23 @@
+Template.filter.helpers({
+  filters: function() {
+    return Items.find({ type: this.type });
+  }
+});
+
+Template.filter.events({
+  'click input[type=checkbox]': function(event, template) {
+    var name = event.target.name;
+    var value = event.target.value;
+
+    if ($(event.target).is(':checked'))
+      Router.query.add(name, value);
+    else
+      Router.query.remove(name, value);
+  }
+});
+
+Template.filterItem.helpers({
+  isChecked: function(name, _id) {
+    return Router.query.isSet(name, _id);
+  }
+});
